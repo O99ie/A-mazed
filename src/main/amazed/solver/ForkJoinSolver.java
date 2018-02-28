b9c433dd4ef6654cf4bcecad0f7347cdba7fd09c
ostacke <olle.stacke@gmail.com>
2018-02-28 16:41:27 +0100

    first

---
+++ src/main/amazed/solver/ForkJoinSolver.java
@@ -0,0 +1,95 @@
+package amazed.solver;
+
+import amazed.maze.Maze;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * <code>ForkJoinSolver</code> implements a solver for
+ * <code>Maze</code> objects using a fork/join multi-thread
+ * depth-first search.
+ * <p>
+ * Instances of <code>ForkJoinSolver</code> should be run by a
+ * <code>ForkJoinPool</code> object.
+ */
+
+
+public class ForkJoinSolver
+    extends SequentialSolver
+{
+    /**
+     * Creates a solver that searches in <code>maze</code> from the
+     * start node to a goal.
+     *
+     * @param maze   the maze to be searched
+     */
+    public ForkJoinSolver(Maze maze)
+    {
+        super(maze);
+        ForkJoinSearch(maze);
+    }
+    
+    public List<Integer> ForkJoinSearch(Maze maze){
+        return ForkJoinSearch(maze, new HashMap<>());
+    }
+
+    public List<Integer> ForkJoinSearch(Maze maze, Map<Integer, Integer> predecessor){
+        int player = maze.newPlayer(start);
+        int current = start;        
+        
+            // If we stand on goal
+            if(hasGoal(current)){
+                return pathFromTo(start, current);
+            }
+            switch(size(maze.neighbours(current))){ // TODO ta bort visited från setet
+                case 0: return null;
+                case 1: maze.move(player, 
+            }
+
+        return null;
+    }    
+
+    /**
+     * Creates a solver that searches in <code>maze</code> from the
+     * start node to a goal, forking after a given number of visited
+     * nodes.
+     *
+     * @param maze        the maze to be searched
+     * @param forkAfter   the number of steps (visited nodes) after
+     *                    which a parallel task is forked; if
+     *                    <code>forkAfter &lt;= 0</code> the solver never
+     *                    forks new tasks
+     */
+    public ForkJoinSolver(Maze maze, int forkAfter)
+    {
+        this(maze);
+        this.forkAfter = forkAfter;
+    }
+
+    /**
+     * Searches for and returns the path, as a list of node
+     * identifiers, that goes from the start node to a goal node in
+     * the maze. If such a path cannot be found (because there are no
+     * goals, or all goals are unreacheable), the method returns
+     * <code>null</code>.
+     *
+     * @return   the list of node identifiers from the start node to a
+     *           goal node in the maze; <code>null</code> if such a path cannot
+     *           be found.
+     */
+    @Override
+    public List<Integer> compute()
+    {
+        return parallelSearch();
+    }
+
+    private List<Integer> parallelSearch()
+    {
+        return null;
+    }
+}
